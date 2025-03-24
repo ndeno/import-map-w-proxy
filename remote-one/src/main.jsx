87eaec2c896e11e4ffdecd7133359ff4c1e3b39b
ndeno <ndeno@users.noreply.github.com>
2025-03-24 22:48:33 +0000

    add remote dep

---
+++ remote-one/src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h3>Remote-one</h3>
+      <span>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </span>
+    </div>
+  );
+}
---
+++ remote-one/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3101,
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.jsx"),
+      formats: ["es"],
+      fileName: () => "index.js",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+    },
+  },
+});
